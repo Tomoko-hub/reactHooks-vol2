0efdcb1716f0adde8b2bfa6a6e184d7898c1c37b
Tomoko <haisaitomoco@gmail.com>
2021-10-11 13:00:45 +0300

    solve prop drilling problem

--- src/components/Events.js
+++ src/components/Events.js
@@ -4,24 +4,24 @@ import Event from "./Event";
 
 import AppContext from "../contexts/AppContext";
 
-const Events = ({ state, dispatch }) => {
-    const value = useContext(AppContext);
+const Events = () => {
+    const { state } = useContext(AppContext);
+    
     return(
         <>
-             <div>{value}</div>
             <h4>Show all event</h4>
             <table className="table table-hover">
-            <thead>
-                <tr>
-                <th>ID</th>
-                <th>Title</th>
-                <th>Body</th>
-                <th></th>
-                </tr>
-            </thead>
-            <tbody>
-                { state.map((event,index) =>(<Event key={index} event={event} dispatch={dispatch} />))}
-            </tbody>
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>Title</th>
+                        <th>Body</th>
+                        <th></th>
+                    </tr>
+                </thead>
+                <tbody>
+                    { state.map((event,index) =>(<Event key={index} event={event} />))}
+                </tbody>
             </table>
         </>
     );
