4f54a48a01125edfc15a6b1dc50df934420c9e0a
Tomoko <haisaitomoco@gmail.com>
2021-10-11 14:22:36 +0300

    refuctor events with reducer

--- src/components/App.js
+++ src/components/App.js
@@ -7,8 +7,11 @@ import Events from './Events';
 import AppContext from '../contexts/AppContext';
 import reducer from '../reducers';
 
-function App() {
-  const [ state, dispatch ] = useReducer(reducer, []);
+const App = () => {
+  const initialState = {
+    events: []
+  }
+  const [ state, dispatch ] = useReducer(reducer, initialState);
 
   return (
     <AppContext.Provider value={{ state, dispatch }}>
